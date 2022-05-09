a936ebe82c7b48280498de4b1bbbc445aa905344
s284858 <s284858@shhaC150M18.ocsb.local>
2022-05-09 12:09:51 -0400

    Step 2

---
+++ Python strings.py
@@ -0,0 +1,24 @@
+"""
+Strings are a datatype.
+Strings can be changed.
+Strings are written within a single or double quotation marks.
+All user input are strings.
+"""
+
+string1 = "This is a string. "
+
+print(string1)
+
+print(string1[0])
+
+print(string1[11:])
+
+print (string1)
+
+var1 = ord('a')
+print(var1)
+
+var2 = chr(994)
+print(var2)
+
+#print("You entered: " + var1)
